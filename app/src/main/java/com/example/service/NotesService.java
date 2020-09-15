cd609173318eaf453753a651c149b5269899d15a
barakg <barakgonen9@gmail.com>
2020-09-15 20:07:44 +0300

    client init

---
+++ app/src/main/java/com/example/service/NotesService.java
@@ -0,0 +1,11 @@
+package com.example.service;
+
+import java.net.http.HttpClient;
+
+public class NotesService {
+    private static String URL = "127.0.0.1:8080/notes";
+
+    public static void fetchDate(){
+
+    }
+}
